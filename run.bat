44156e30e690ee947a6d61b826913bcfb1d32708
Valmir <valmir.e@gmx.de>
2019-09-06 16:53:53 +0200

    changed run script

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-docker run --rm --volume %cd%/src:/home/jovyan/work --name deeplearning -p 8888:8888 jupyter/tensorflow-notebook:latest
+docker run --rm --volume %cd%/notebooks:/home/jovyan/work --name deeplearning -p 8888:8888 jupyter/tensorflow-notebook:latest
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker run --rm --volume $pwd/src:/home/jovyan/work --name deeplearning -p 8888:8888 jupyter/tensorflow-notebook:latest
+sudo docker run --rm --volume $(pwd)/notebooks:/home/jovyan/work --name deeplearning -p 8888:8888 jupyter/tensorflow-notebook:latest
